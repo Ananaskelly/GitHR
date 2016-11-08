632fc1ec8241f55e48c2257fb7ac07f719efba1a
Ananasy <anastasiya_johnni@mail.ru>
2016-11-08 22:23:34 +0300

    ypu can select languages now

--- static/app/controllers/mainCtrl.js
+++ static/app/controllers/mainCtrl.js
@@ -2,6 +2,7 @@
 angular.module('app.controller')
     .controller('mainCtrl', function($scope, Content, $state){
 		Content.getRights().then(function(response){
+		console.log(response);
 			if (JSON.parse(response.toLowerCase())) {
 				$state.go('search');
 			}
--- static/app/controllers/userProfileCtrl.js
+++ static/app/controllers/userProfileCtrl.js
@@ -1,22 +1,66 @@
 angular.module('app.controller')
-	.controller('userProfileCtrl', function($scope, Content, $stateParams, $http){
-		var login = $stateParams.login;
+	.controller('userProfileCtrl', function($scope, Content, $stateParams){
+		var repos = {};
+		$scope.shownRepos = [];
 		$scope.user = {};
+		$scope.selectedLangs = [];
+		var selected = 0;
+		var login = $stateParams.login;
+		var langs = {};
+		$scope.user.relevantLangs = [];
 		Content.getUserInfo(login).then(function(response){
 			$scope.user = response;
 		});
-		$scope.repos = {};
 		Content.getUserRepos(login).then(function(response){
-			$scope.repos = response;
+			repos = response;
+			for (var key in response){
+				$scope.shownRepos.push(response[key]);
+				var lang = response[key].language;
+				if (langs.hasOwnProperty(lang)) {
+					langs[lang].amount ++;
+					langs[lang].associatedKeys.push(key);
+				} else {
+					langs[lang] = {
+						'amount': 1,
+						'associatedKeys': [key]
+					};
+				}
+			}
+			$scope.user.relevantLangs = Object.keys(langs).sort(
+				function(a,b){
+					return langs[b].amount-langs[a].amount
+			});
 		})
-		$http({
-                    method: 'GET',
-                    url: '/api/repos/tfoote'
-                }).success(function (response) {
-                    console.log(response);
-                })
-                    .error(function (error, status) {
-                       
-                    });
-                
+		$scope.selectByLang = function(index){
+			var choosenLang = $scope.user.relevantLangs[index];
+			if ($scope.selectedLangs.indexOf(choosenLang) !== -1){
+				return 
+			}
+			$scope.selectedLangs.push(choosenLang);
+			if (selected === 0){
+				$scope.shownRepos.length = 0;
+			}
+			selected ++;
+			for (var i=0; i<langs[choosenLang].associatedKeys.length; i++) {
+				$scope.shownRepos.push(repos[langs[choosenLang].associatedKeys[i]]);
+			}
+			console.log(langs[choosenLang].associatedKeys);
+		}
+		
+		$scope.deleteFromSet = function(index){
+			var choosenLang = $scope.selectedLangs[index];
+			$scope.selectedLangs.splice(index,1);
+			selected --;
+			if (selected === 0){
+				$scope.shownRepos.length = 0;
+				for (var key in repos){
+					$scope.shownRepos.push(repos[key]);
+				}
+			}
+			for (var i=0; i<langs[choosenLang].associatedKeys.length; i++) {
+				var repInd = $scope.shownRepos.indexOf(repos[langs[choosenLang].associatedKeys[i]])
+				$scope.shownRepos.pop(repInd,1);
+			}
+			
+		}		
 	})
